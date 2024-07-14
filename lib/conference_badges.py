7dd3833a0ce1596c9040b599166ddff20b6cd15f
Miguel Soriano <miguel.sor.88@gmail.com>
2024-07-15 00:23:13 +1000

    completed lab writing for loops and accessing items in list

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+
 
 def batch_badge_creator(names):
-    return None
+    creator_list = []
+    for name in names:
+        creator_list.append(f"Hello, my name is {name}.")
+    return creator_list
+
 
 def assign_rooms(names):
-    return None
+    creator_list_with_room = []
+    for index, name in enumerate(names):
+        creator_list_with_room.append(
+            f"Hello, {name}! You'll be assigned to room {index + 1}!")
+    return creator_list_with_room
+
 
 def printer(names):
-    return None
+    list = batch_badge_creator(names)
+    list_2 = assign_rooms(names)
+    for item in list:
+        print(item)
+    for item in list_2:
+        print(item)
